d51196a7fe7f8b63b1a9df0e4a226d7345940a16
R <renaud.genestier@gmail.com>
2025-01-24 16:02:03 +0100

    exercice 3 promesses

---
+++ promise-it-wont-hurt.js
@@ -0,0 +1,8 @@
+var promise = new Promise(function (fulfill, reject) {
+    setTimeout(() => reject(new Error('REJET !')), 300);
+});
+
+function onReject (error) {
+    console.log(error.message);
+}
+promise.then(null, onReject);
