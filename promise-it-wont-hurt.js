922086cf42e8a24b79f88bebc4177bd42814163b
R <renaud.genestier@gmail.com>
2025-01-28 09:23:41 +0100

    fin des exos sur la promesse

--- promise-it-wont-hurt.js
+++ promise-it-wont-hurt.js
@@ -1,10 +1,13 @@
-var promise = new Promise( function (fulfill, reject) {
-    fulfill("J'AI ETE APPELEE");
-    reject(new Error("JE N'AI PAS ETE RAPPELER"));
-});
+// Créer une promesse accomplie avec Promise.resolve
+const promise = Promise.resolve('Succès !');
 
-function onRejected(error) {
-    console.log(error.message);
-}
-
-promise.then(console.log, onRejected);
+// Simuler une erreur après un succès
+promise
+    .then((result) => {
+        console.log(result); // Affiche "Succès !"
+        throw new Error('Quelque chose a foiré !'); // On force une erreur
+    })
+    .catch((err) => {
+        console.error('IL Y A UN SOUCI !');
+        console.error(err.message); // Affiche "Quelque chose a foiré !"
+    });
